f433c11a2244e0a1e1b7d8de23053e05959f6a1a
James Tu <jamestvvo@gmail.com>
2025-03-06 11:26:00 -0500

    added libgdx-ai library

---
+++ core/src/edu/cornell/gdiac/b2lights/AISteeringState.java
@@ -0,0 +1,9 @@
+package edu.cornell.gdiac.b2lights;
+import com.badlogic.gdx.ai.steer.Steerable;
+import com.badlogic.gdx.ai.steer.behaviors.Arrive;
+import com.badlogic.gdx.ai.utils.Location;
+import com.badlogic.gdx.math.Vector2;
+
+public class AISteeringState {
+
+}
