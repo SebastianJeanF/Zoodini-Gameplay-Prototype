102aeeda6f118ca49a1fcb263e18ed16310938ca
Sebastian Jean-Francois <sebeld9@gmail.com>
2025-03-06 10:56:28 -0500

    Guard prioritizes chasing over following meow spot

--- core/src/edu/cornell/gdiac/b2lights/Guard.java
+++ core/src/edu/cornell/gdiac/b2lights/Guard.java
@@ -3,7 +3,7 @@ package edu.cornell.gdiac.b2lights;
 import com.badlogic.gdx.math.Vector2;
 
 public class Guard extends DudeModel {
-    public static final int MAX_CHASE_TIME = 180;
+    public static final int MAX_CHASE_TIME = 60; // 1 second
     public static final float FOV_DISTANCE = 7.0f; // Maximum detection distance.
     public static final float FOV_ANGLE = 45.0f; // Total cone angle in degrees.
 
@@ -42,37 +42,44 @@ public class Guard extends DudeModel {
     /** The position that this guard should move to */
     Vector2 target = null;
 
+    /** If a guard is "agroed", it is currently chasing a player */
     public boolean isAgroed() {
         return isChasing;
     }
 
+    /** The value of target is only valid if guard is agroed or is "meowed" */
     public Vector2 getTarget() {
+        if (meowed == true) {
+            System.out.print("Guard is getting meow target");
+        }
         return target;
     }
 
     public void setTarget(Vector2 target) {
+
         this.target = target;
     }
 
-    public void setAgroed(Vector2 target, boolean meowed) {
-        isChasing = true;
-        this.target = target;
-        this.meowed = meowed;
+    public void setAgroed(boolean agroed) {
+        isChasing = agroed;
     }
 
     public void setMeow(boolean meowed) {
         this.meowed = meowed;
     }
-    public void deAgro() {
-        isChasing = false;
-        target = null;
-        meowed = false;
-    }
 
+
+    /** If a guard is "meowed", it is currently patrolling to the spot of the meow,
+     * but they are not chasing a player. When either alerted by a security camera,
+     * or if they see a player, or if they reach the spot of the meow, the guard
+     * leaves the "meowed" state
+     * */
     public boolean isMeowed() {
         return meowed;
     }
 
+    /** This timer is used to determine how long a guard should chase a player
+     * before giving up and returning to their patrol route */
     public int getChaseTimer() {
         return chaseTimer;
     }
